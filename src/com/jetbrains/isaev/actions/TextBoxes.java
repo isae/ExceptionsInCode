fe022c775980070a4385c694b7a86acc842681b4
Ilya.Isaev <123>
2014-08-06 13:18:27 +0400

    added needful files

---
+++ src/com/jetbrains/isaev/actions/TextBoxes.java
@@ -0,0 +1,27 @@
+package com.jetbrains.isaev.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+
+/**
+ * User: Xottab
+ * Date: 18.07.2014
+ */
+public class TextBoxes extends AnAction {
+
+    public TextBoxes() {
+        super("Text _Boxes");
+    }
+
+    public void actionPerformed(AnActionEvent event) {
+        Project project = event.getData(PlatformDataKeys.PROJECT);
+        String txt = Messages.showInputDialog(project, "What is your name?", "Input your name", Messages.getQuestionIcon());
+        Messages.showMessageDialog(project, "Hello, " + txt + "!\n I am glad to see you.", "Information", Messages.getInformationIcon());
+
+    }
+
+
+}
