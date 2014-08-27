cb38fda049c6ba6323b796577f20b382be64be4b
Ilya.Isaev <123>
2014-08-27 11:44:29 +0400

    commit before massive refactoring( drag&drop + inner classes processing)

---
+++ src/testSrc/NestedExceptionTest.java
@@ -0,0 +1,54 @@
+package testSrc;
+
+/**
+ * Created by Ilya.Isaev on 26.08.2014.
+ */
+public class NestedExceptionTest {
+    static class A {
+        class B {
+            class C {
+                void test() throws Exception {
+                    new Runnable() {
+                        @Override
+                        public void run() {
+
+                        }
+                    }.run();
+                    new Runnable() {
+                        void run2() {
+                        }
+
+                        @Override
+                        public void run() {
+                            class D {
+                                void test3() {
+                                    A aa = null;
+                                    aa.b.c.test2();
+                                }
+                            }
+                            class E {
+                                void test3() {
+                                    A aa = null;
+                                    aa.b.c.test2();
+                                }
+                            }
+                            new E().test3();
+                        }
+                    }.run();
+                }
+
+                void test2() {
+
+                }
+            }
+
+            C c = new C();
+        }
+
+        B b = new B();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new A().b.c.test();
+    }
+}
